34f92d6e8b17dcc80367fa9ee8973d9dc984f9bc
ab-nano <husseny.xyz@gmail.com>
2019-12-08 23:03:49 +0100

    array manipulation challenges

--- array_manipulation.py
+++ array_manipulation.py
@@ -3,78 +3,22 @@
 # Array Manipulation
 
 # Method 1
-#
+# Efficient Solution. Passes all test cases
 
 
-def arrayManipulation(maximum, queries):
+def arrayManipulation(n, queries):
+    arr = [0]*n
+    for operation in queries:
+        arr[operation[0]-1] += operation[2]
+        if operation[1] < n:
+            arr[operation[1]] -= operation[2]
     maxi = 0
-    for i, operation in enumerate(queries):
-        operation_start = operation[0]
-        operation_end = operation[1]
-        operation_value = operation[2]
-        for j, comparing_operation in enumerate(queries, start=1):
-            comparing_operation_start = comparing_operation[0]
-            comparing_operation_end = comparing_operation[1]
-            comparing_operation_value = comparing_operation[2]
-            # The case where the sets are disjoint
-            if comparing_operation_start > operation_end or operation_start > comparing_operation_end:
-                next
-            # The case where the sets are exactly equivalent
-            if comparing_operation_start == operation_start and comparing_operation_end == operation_end:
-                new_value = operation_value + comparing_operation_value
-                comparing_operation[2] = new_value
-                if new_value > maxi:
-                    maxi = new_value
-                del queries[i]
-                break
-            # check which operation starts first
-            if comparing_operation_start > operation_start:
-                # The case where the sets intersect
-                if comparing_operation_end > operation_end:
-                    queries.append(
-                        [operation_start, comparing_operation_start-1, operation_value])
-
-                    comparing_operation[1] = operation_end
-                    new_value = operation_value + comparing_operation_value
-                    comparing_operation[2] = new_value
-
-                    queries.append(
-                        [operation_end+1, comparing_operation_end, comparing_operation_value])
-
-                    if new_value > maxi:
-                        maxi = new_value
-                    del queries[i]
-                    break
-                elif comparing_operation_end == operation_end:
-                    queries.append(
-                        [operation_start, comparing_operation_start-1, operation_value])
-
-                    new_value = operation_value + comparing_operation_value
-                    comparing_operation[2] = new_value
-
-                    if new_value > maxi:
-                        maxi = new_value
-
-                else:
-                    queries.append(
-                        [operation_start, comparing_operation_start-1, operation_value])
-
-                    new_value = operation_value + comparing_operation_value
-                    comparing_operation[2] = new_value
-
-                    queries.append(
-                        [comparing_operation_end+1, operation_end, comparing_operation_value])
-
-                    if new_value > maxi:
-                        maxi = new_value
-
-            # elif comparing_operation_start == operation_start:
-
-            # else:
-
-                #
-            # if (comparing_operation_start >= operation_start and comparing_operation_end <= operation_end) or
-            # (operation_start)
+    count = 0
+    for item in arr:
+        count += item
+        if count > maxi:
+            maxi = count
+    return maxi
 
 
 # Method 2
